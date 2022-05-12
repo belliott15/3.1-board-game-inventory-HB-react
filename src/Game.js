fdf55e1b1cdaa4897b9ac4cb029cd5b33359461d
Beau Elliott <belliott1516@gmail.com>
2022-05-12 09:50:48 -0700

    completed detail page and got it to render

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Game({ game }) {
   return (
     // be sure this component is wrapped in a react-router link that takes the user to the correct detail page
-    <Link to={`/detail/${game.id}`}>
+    <Link to={`/board-games/${game.id}`}>
       <div className='game'>
         <h3>{game.title}</h3>
         <p>A {game.genre} game by designer {game.designer}</p>
