0f22ac979fbb005f185d35838c4ba653d682ce4a
Beau Elliott <belliott1516@gmail.com>
2022-05-11 20:39:54 -0500

    created ternery for app.js to redirect if no token

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ export default function App() {
   // You'll need to track the user in state
   const [email, setEmail] = useState();
   const [token, setToken] = useState();
+  const [user, setUser] = useState();
   // add a useEffect to get the user and inject the user object into state on load
   useEffect(() => {
     const user = getUser();
@@ -30,6 +31,9 @@ export default function App() {
   }, []);
   async function handleLogout() {
     // call the logout function
+    await logout();
+    setEmail('');
+    setToken('');
     // clear the user in state
   }
 
@@ -38,20 +42,42 @@ export default function App() {
       <div className='App'>
         <header>
           {/* if there is a user in state, render out a link to the board games list, the create page, and add a button to let the user logout */}
+          {user ? 
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="/board-games">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/create">Board Games</NavLink>
+                </li>
+                <li>
+                  <button onClick={handleLogout}>Log out</button>
+                </li>
+              </ul>
+            </nav> 
+            : 
+            <AuthPage />
+          }
         </header>
         <main>
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the board games list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {token ? <Redirect to='/board-games' /> 
+                : <AuthPage setEmail={setEmail} setToken={setToken}/>}
             </Route>
             <Route exact path="/board-games">
               {/* if there is a user, render the board games list. Otherwise, redirect to the home route/auth page */}
+              {token ? <ListPage /> : <Redirect to='/' />}
             </Route>
             <Route exact path="/board-games/:id">
               {/* if there is a user, render the detail page. Otherwise, redirect to the home route/auth page */}
+              {token ? <DetailPage /> : <Redirect to='/' />}
             </Route>
             <Route exact path="/create">
               {/* if there is a user, render the create page. Otherwise, redirect to the home route/auth page */}
+              {token ? <CreatePage /> : <Redirect to='/' />}
             </Route>
           </Switch>
         </main>
