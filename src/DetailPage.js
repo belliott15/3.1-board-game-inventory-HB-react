b74456b09aa607efd7bbb40aeac4470025c76a1f
Beau Elliott <belliott1516@gmail.com>
2022-05-12 10:04:03 -0700

    changed detail page to display like create game but lets you edit

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getGameById } from './services/fetch-utils';
+import { useParams, useHistory } from 'react-router-dom';
+import { getGameById, updateGame } from './services/fetch-utils';
 
 export default function DetailPage() {
   const { id } = useParams();
+  const { push } = useHistory();
   const [game, setGame] = useState({
     title: '',
     genre: '',
@@ -21,16 +22,82 @@ export default function DetailPage() {
     }
     gameDetail();
   }, [id]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    // create a game
+    await updateGame(game, id);
+    
+    // use history.push to send the user to the list page
+    push('/board-games');
+  }
+
   // refactor this component to be a form that hydrates a form with data using the id from the URL params in react-router
   // this form should let the user update the item on submit
   return (
     <div className='detail'>
-      <h1>{game.title}</h1>
-      <h2>A {game.genre} game for {game.min_players} to {game.max_players} players</h2>
-      <h3>by celebrated designer {game.designer}</h3>
-      <p>
-        {game.description}
-      </p>
+      <form onSubmit={handleSubmit}>
+        <h2>Add board game</h2>
+        <label>
+            Title
+          {/* on change, set the title in state */}
+          <input value={game.title} onChange={(e) => setGame({
+            ...game, 
+            title: e.target.value,
+          })} required name='title' />
+        </label>
+        <label>
+            Genre
+          {/* on change, set the genre in state */}
+          <select value={game.genre} onChange={(e) => setGame({
+            ...game,
+            genre: e.target.value,
+          })
+          } required>
+            <option>Tile-laying</option>
+            <option>Economic</option>
+            <option>War</option>
+            <option>Card</option>
+            <option>Abstract</option>
+            <option>Cooperative</option>
+            <option>Solo</option>
+          </select>
+        </label>
+        <label>
+            Designer
+          {/* on change, set the designer in state */}
+          <input value={game.designer} onChange={(e) => setGame({
+            ...game, 
+            designer: e.target.value,
+          })} required name='designer' />
+        </label>
+        <label>
+            Min Players
+          {/* on change, set the min players in state */}
+          <input value={game.min_players} onChange={(e) => setGame({
+            ...game, 
+            min_players: e.target.value,
+          })}required name='min_players' />
+        </label>
+        <label>
+            Max Players
+          {/* on change, set the max players in state */}
+          <input value={game.max_players} onChange={(e) => setGame({
+            ...game, 
+            max_players: e.target.value,
+          })}required name='max_players' />
+        </label>
+        <label>
+            Description
+          {/* on change, set the description in state */}
+          <textarea value={game.description} onChange={(e) => setGame({
+            ...game, 
+            description: e.target.value,
+          })}required name='max_players' />
+        </label>
+        <button>Create game</button>
+      </form>
     </div>
   );
 }
